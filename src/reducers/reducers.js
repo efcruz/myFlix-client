093cfcbf2136574ed0b2b9de16b87e589fd4edcd
epcruz <eduardaffpaulo@gmail.com>
2022-05-23 08:53:57 +0200

    fix reducers

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -4,38 +4,26 @@ import { SET_MOVIES, SET_FILTER } from '../actions/actions';
 
 //return the text input in the search bar
 function visibilityFilter(state = '', action) {
-    /*
-    if (action.type === 'SET_FILTERS') {
-        return action.value;
-    } else {
-        return state;
-    }
-    */
-
     switch (action.type) {
-        case "SET_FILTERS":
+        case SET_FILTER:
             return action.value;
         default:
             return state;
     }
 }
+
 
 function movies(state = [], action) {
-    /*
-    if (action.type === 'SET_MOVIES') {
-        return action.value;
-    } else {
-        return state;
-    }
-    */
-
     switch (action.type) {
-        case "SET_MOVIES":
+        case SET_MOVIES:
             return action.value;
         default:
             return state;
     }
 }
+
+
+
 
 const moviesApp = combineReducers({
     visibilityFilter,
